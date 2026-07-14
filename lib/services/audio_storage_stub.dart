69a4f28a9ed0f9b96bae876834de954d1b28d6c0
weenukarajapaksha <weenuka20030816@gmail.com>
2026-07-15 00:42:33 +0530

    Update audio_storage_stub.dart

--- lib/services/audio_storage_stub.dart
+++ lib/services/audio_storage_stub.dart
@@ -3,3 +3,5 @@ import 'dart:typed_data';
 Future<String> resolveRecordTarget(String fileName) async => fileName;
 
 Future<Uint8List?> captureBytes(String recordedPath) async => null;
+
+Future<void> deleteRecordedFile(String? filePath) async {}
